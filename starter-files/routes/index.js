0a08e1e43c8742c66d49e208378692645b4ea7ec
Timothy O Johnson <pavementii@gmail.com>
2018-04-03 09:28:46 -0400

    intial commit

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
+  const wes = {name: 'Tim', age: 100, cool: true}
+  console.log("what up tho?!")
   res.send('Hey! It works!');
+  //res.json(wes)
+  // res.send(req.query.name)
+  //res.send(req.query)
 });
+
+router.get('/reverse/:name',(req, res) => {
+  const reverse = [...req.params.name].reverse().join('')
+  res.send(reverse)
+})
 
 module.exports = router;
